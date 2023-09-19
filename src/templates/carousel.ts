db0b54cc072e4db802c7dc9c628cc65072497ddb
Sean Wong <seanwong24@live.com>
2023-09-19 19:48:41 +0000

    update carousel

--- src/templates/carousel.ts
+++ src/templates/carousel.ts
@@ -8,22 +8,41 @@ type Props = {
 const Carousel = ({ langHelper }: Props) => html`
   <style>
     .carousel-item {
-      display: flex;
-      align-items: center;
-      justify-content: center;
+      position: relative;
+    }
+
+    .carousel-text {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
       font-size: 2rem;
+      background: inherit;
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+      filter: invert(1) grayscale(1);
     }
   </style>
 
   <gaia-carousel timeout="5" style="height: 500px">
-    <div class="carousel-item" style="background: beige">
-      ${langHelper("一", "One")}
+    <div
+      class="carousel-item"
+      style="background: url(https://picsum.photos/id/1/1440/900)"
+    >
+      <span class="carousel-text"> ${langHelper("一", "One")} </span>
     </div>
-    <div class="carousel-item" style="background: bisque">
-      ${langHelper("二", "Two")}
+    <div
+      class="carousel-item"
+      style="background: url(https://picsum.photos/id/356/1440/900)"
+    >
+      <span class="carousel-text"> ${langHelper("二", "Two")} </span>
     </div>
-    <div class="carousel-item" style="background: pink">
-      ${langHelper("三", "Three")}
+    <div
+      class="carousel-item"
+      style="background: url(https://picsum.photos/id/2/1440/900)"
+    >
+      <span class="carousel-text"> ${langHelper("三", "Three")} </span>
     </div>
   </gaia-carousel>
 `;
